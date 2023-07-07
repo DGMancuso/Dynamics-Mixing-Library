637cf90ab48235836d414f17cd5be33c7d7555c8
DGMancuso <138873062+DGMancuso@users.noreply.github.com>
2023-07-07 11:53:14 -0500

    setSigns added

--- DynamicsMixingLibrary.cpp
+++ DynamicsMixingLibrary.cpp
@@ -3,35 +3,34 @@
 #define POS_DML true
 #define NEG_DML false
 
+bool setSigns(bool &a, bool &b, uint8_t &type)
+{
+    switch (type){
+        case 0: 
+            a = POS_DML; b = POS_DML; break;
+        case 1:
+            a = POS_DML; b = NEG_DML; break;
+        case 3:
+            a = NEG_DML; b = POS_DML; break;
+        case 4:
+            a = NEG_DML; b = NEG_DML; break;
+        default:
+            return false;
+    }
+    return true;
+}
+
+
 bool simpleMix(uint8_t &inA, 
                uint8_t &inB, 
-               uint8_t type, 
+               uint8_t &type, 
                uint8_t zeroPoint1, 
                uint8_t zeroPoint2)
 {
     int16_t orig1 = inA - zeroPoint1;
     int16_t orig2 = inB - zeroPoint2;
     bool sign1, sign2;
-    switch (type){
-        case 0: 
-            sign1 = POS_DML;
-            sign2 = POS_DML;
-            break;
-        case 1:
-            sign1 = POS_DML;
-            sign2 = NEG_DML;
-            break;
-        case 3:
-            sign1 = NEG_DML;
-            sign2 = POS_DML;
-            break;
-        case 4:
-            sign1 = NEG_DML;
-            sign2 = NEG_DML;
-            break;
-        default:
-            return false;
-    }
+    setSigns(sign1, sign2, type);
     inA = zeroPoint1 + (((sign1 ? orig1 : -orig1) + (sign2 ? orig2 : -orig2))/2);
     inB = zeroPoint1 + (((!sign1 ? orig1 : -orig1) + (!sign2 ? orig2 : -orig2))/2);
     return true;
@@ -39,7 +38,7 @@ bool simpleMix(uint8_t &inA,
 
 bool fullThrottleMix(uint8_t &thrIn, 
                      uint8_t &inB, 
-                     uint8_t type, 
+                     uint8_t &type, 
                      uint8_t zeroPointThr, 
                      uint8_t zeroPoint2)
 {
--- DynamicsMixingLibrary.h
+++ DynamicsMixingLibrary.h
@@ -2,15 +2,17 @@
 #define DYNAMICS_MIXING_LIBRARY
 #include <Arduino.h>
 
+bool setSigns(bool &a, bool &b, uint8_t &type);
+
 bool simpleMix(uint8_t &inA, 
                uint8_t &inB, 
-               uint8_t type, 
+               uint8_t &type, 
                uint8_t zeroPoint1 = 150,  //defaults to 1.5ms
                uint8_t zeroPoint2 = 150); //defaults to 1.5ms
 
 bool fullThrottleMix(uint8_t &thrIn, 
                      uint8_t &inB, 
-                     uint8_t type, 
+                     uint8_t &type, 
                      uint8_t zeroPointThr = 150,  //defaults to 1.5ms
                      uint8_t zeroPoint2 = 150); //defaults to 1.5ms
 
