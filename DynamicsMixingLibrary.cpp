e66c60eaa813f2187b2e94af66676074c766eb3c
DGMancuso <138873062+DGMancuso@users.noreply.github.com>
2023-07-09 10:50:14 -0500

    fixing typing

--- DynamicsMixingLibrary.cpp
+++ DynamicsMixingLibrary.cpp
@@ -5,7 +5,7 @@
 // sets directions of each channel from midpoint based on arbitrary type
 // this allows servo reversing in the mixing function
 // choose correct type based on needed functionallity
-bool setSigns(int8_t &a, int8_t &b, uint8_t &type)
+bool setSigns(int8_t &a, int8_t &b, uint8_t type)
 {
     switch (type){
         case 0: 
@@ -26,11 +26,10 @@ bool setSigns(int8_t &a, int8_t &b, uint8_t &type)
 
 bool averageMix(int8_t &in1, 
                 int8_t &in2, 
-                uint8_t &type)
+                uint8_t type)
 {
     int8_t orig1 = in1;
     int8_t sign1, sign2; setSigns(sign1, sign2, type);      // set directions of outputs using these variables: -1 & 1 are only values
-    //FIXME
     in1 = ((sign1 * orig1 + sign2 * in2)/2);
     in2 = ((-sign1 * orig1 + -sign2 * in2)/2);
     return true;
@@ -38,9 +37,8 @@ bool averageMix(int8_t &in1,
 
 bool maxMix(int8_t &in1, 
             int8_t &in2, 
-            uint8_t &type)
+            uint8_t type)
 {
-    //FIXME
     int8_t orig1 = in1;
     int8_t sign1, sign2;
     setSigns(sign1, sign2, type);                           // set directions of outputs using these variables: -1 & 1 are only values
@@ -51,21 +49,20 @@ bool maxMix(int8_t &in1,
 
 bool fullThrottleMix(int8_t &thrIn, 
                      int8_t &in2, 
-                     uint8_t &type)
+                     uint8_t type)
 {
-    //FIXME
     int8_t origThr = thrIn;
     int8_t orig2 = in2;
     int8_t sign1, sign2; setSigns(sign1, sign2, type);      // set directions of outputs using these variables: -1 & 1 are only values
-    thrIn = min(sign1 * origThr + sign2 * in2, 100);
-    in2 = min((-sign1 * origThr)/2 + (-sign2 * in2)/2, 100);
+    thrIn = min(sign1 * origThr + sign2 * in2/2, 100);
+    in2 = min(-sign1 * origThr + -sign2 * in2/2, 100);
     return false;
 }
 
 bool dualScrewMix(int8_t &thrLIn, // Assumed this is the single input for throttle
                   int8_t &thrRIn, // Only an output
                   int8_t &rudIn, 
-                  uint8_t &type,
+                  uint8_t type,
                   bool flipDelta)
 {
     float gamma = 1.2;                                                          // exponent for throttle difference calculation
--- DynamicsMixingLibrary.h
+++ DynamicsMixingLibrary.h
@@ -2,14 +2,14 @@
 #define DYNAMICS_MIXING_LIBRARY
 #include <Arduino.h>
 
-bool setSigns(int8_t &a, int8_t &b, uint8_t &type);
+bool setSigns(int8_t &a, int8_t &b, uint8_t type);
 
 // averages the channels together
 // 100 output only if both inputs are 100 (stick to corner)
 // 50 output if one channel maxxed (stick full forward) 
 bool averageMix(int8_t &in1, 
                 int8_t &in2, 
-                uint8_t &type); 
+                uint8_t type); 
       
 // doubles average mix gain
 // 100 output on BOTH channels if one input maxxed (stick full forward) 
@@ -17,18 +17,18 @@ bool averageMix(int8_t &in1,
 // Outside this 'diamond', signal saturates at 100 - lose 50% resolution
 bool maxMix(int8_t &in1, 
             int8_t &in2, 
-            uint8_t &type); 
+            uint8_t type); 
 
 // mix for maxMix in one stick direction and average mix in other direction 
 bool fullThrottleMix(int8_t &thrIn, 
                      int8_t &in2, 
-                     uint8_t &type); 
+                     uint8_t type); 
 
 //arbitrarily tuned mix for control of twin screws with rudder
 bool dualScrewMix(int8_t &thrLIn, 
                   int8_t &thrRIn, 
                   int8_t &rudIn, 
-                  uint8_t &type, 
+                  uint8_t type, 
                   bool flipDelta = false); 
 
 #endif // DYNAMICS_MIXING_LIBRARY
